d9865ea7b7074c00d64332cc786c8d75ef7f00e2
Jahidul Islam <ziopu05@gmail.com>
2021-04-21 12:28:17 +0600

    second commit

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Players.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee,faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 const Players = (props) => {
     const {name,img,salary, id}= props.player;
     const handleAddPlayer = props.handleAddPlayer;
@@ -9,7 +10,7 @@ const Players = (props) => {
             <img className="playerImg" src={img} alt=""/>
             <h4>Name: {name}</h4>
             <p>Salary: {salary} </p>
-            <button className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)}>Select</button>
+            <button className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)}> <FontAwesomeIcon icon={faPlusCircle} /> Select</button>
         </div>
     );
 };
